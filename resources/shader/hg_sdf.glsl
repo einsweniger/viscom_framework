17781d3ea8cfd0bebaf41b07c01e4fbad7ecba84
David '-1' Schmid <gdkags@gmail.com>
2017-08-31 18:49:06 +0200

    replaced macro

--- resources/shader/hg_sdf.glsl
+++ resources/shader/hg_sdf.glsl
@@ -276,7 +276,7 @@ float fCapsule(vec3 p, float r, float c) {
 // Distance to line segment between <a> and <b>, used for fCapsule() version 2below
 float fLineSegment(vec3 p, vec3 a, vec3 b) {
 	vec3 ab = b - a;
-	float t = saturate(dot(p - a, ab) / dot(ab, ab));
+	float t = clamp(dot(p - a, ab) / dot(ab, ab), 0, 1);
 	return length((ab*t + a) - p);
 }
 
