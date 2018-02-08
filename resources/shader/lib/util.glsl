0dc1ed29e4f88cf3d02623bc7c478c935e17a991
David '-1' Schmid <gdkags@gmail.com>
2018-02-08 17:42:49 +0100

    make some noooooise

--- resources/shader/lib/util.glsl
+++ resources/shader/lib/util.glsl
@@ -87,3 +87,28 @@ vec3 sky_color(vec3 ray_dir, vec3 light_dir) {
     base_col = mix(vec3(.3),vec3((ray_dir.y<0.)?0.:1.),abs(ray_dir.y));
     return base_col*d2;
 }
+
+// Noise by iq
+float hash( float n ) { return fract(sin(n)*753.5453123); }
+float noise( in vec3 x )
+{
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+    f = f*f*(3.0-2.0*f);
+
+    float n = p.x + p.y*157.0 + 113.0*p.z;
+    return mix(mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+                   mix( hash(n+157.0), hash(n+158.0),f.x),f.y),
+               mix(mix( hash(n+113.0), hash(n+114.0),f.x),
+                   mix( hash(n+270.0), hash(n+271.0),f.x),f.y),f.z);
+}
+
+float fbm(vec3 x)
+{
+    float ret = noise(x);
+    ret += noise(x * 2.0) / 2.0;
+    ret += noise(x * 4.0) / 4.0;
+    ret += noise(x * 8.0) / 8.0;
+    ret += noise(x * 16.0) / 16.0;
+    return ret;
+}
