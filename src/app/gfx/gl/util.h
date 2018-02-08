cae20523e84ecd91defc3a8eb47beb7293146be6
David '-1' Schmid <gdkags@gmail.com>
2018-02-08 17:49:44 +0100

    add booleans to getSize()

--- src/app/gfx/gl/util.h
+++ src/app/gfx/gl/util.h
@@ -281,15 +281,19 @@ namespace viscom {
             {
                 case gl::GL_FLOAT:
                 case gl::GL_INT:
+                case gl::GL_BOOL:
                     return 1;
                 case gl::GL_FLOAT_VEC2:
                 case gl::GL_INT_VEC2:
+                case gl::GL_BOOL_VEC2:
                     return 2;
                 case gl::GL_FLOAT_VEC3:
                 case gl::GL_INT_VEC3:
+                case gl::GL_BOOL_VEC3:
                     return 3;
                 case gl::GL_FLOAT_VEC4:
                 case gl::GL_INT_VEC4:
+                case gl::GL_BOOL_VEC4:
                     return 4;
                 default:return 0;
 
