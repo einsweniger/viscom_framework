c9e845d8cf8bfcf6c885488b13ccb1b7e278455c
David '-1' Schmid <david-1.schmid@uni-ulm.de.de>
2018-02-12 15:57:38 +0100

    fix for build with clang

--- cmake/BuildConfig.cmake
+++ cmake/BuildConfig.cmake
@@ -1,6 +1,6 @@
 # Build-flags.
 if(UNIX)
-  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -Wall -Wno-unused-function -Wno-unused-parameter -Wextra -Wpedantic -std=c++17")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -Wall -Wno-unused-function -Wno-unused-parameter -Wextra -Wpedantic -std=c++17 -pg")
   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}")
 
   list(APPEND CORE_LIBS
@@ -11,11 +11,17 @@ if(UNIX)
 		${X11_Xinput_LIB}
 		${X11_Xxf86vm_LIB}
 		${X11_Xcursor_LIB}
-		${CMAKE_THREAD_LIBS_INIT})
+		${CMAKE_THREAD_LIBS_INIT}
+    )
 
   # add compiler-options for AppleClang to ignore linker warnings from assimp on macOS
   if (CMAKE_CXX_COMPILER_ID MATCHES "AppleClang")
     add_compile_options(-fvisibility=hidden -fvisibility-inlines-hidden)
+  elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    # clang does not work well with gcc-stdlib, so this is necessary o_0
+    add_compile_options(-stdlib=libc++)
+    list(APPEND CORE_LIBS c++experimental c++abi)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++")
   endif()
 #elseif(MSVC)
 #  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3 /EHsc /MP /std:c++latest")
