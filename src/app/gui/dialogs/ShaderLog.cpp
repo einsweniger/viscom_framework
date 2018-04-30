fcd4a760493ff08807fe8cafcfdea7fb5fcdb77a
David '-1' Schmid <david-1.schmid@uni-ulm.de.de>
2018-04-30 14:31:21 +0200

    correct shaderlog draw

--- src/app/gui/dialogs/ShaderLog.cpp
+++ src/app/gui/dialogs/ShaderLog.cpp
@@ -19,22 +19,28 @@ namespace minuseins::gui {
     }
 
     void ShaderLog::Draw(const char *title, bool *p_open) {
+        if(!visible) return;
         ImGui::SetNextWindowSize(ImVec2(500,400), ImGuiCond_FirstUseEver);
-        if(ImGui::Begin(title, p_open)) {
-            bool copy = ImGui::Button("Copy");
-            ImGui::SameLine();
-            ImGui::Separator();
-            ImGui::BeginChild("scrolling", ImVec2(0,0), false, ImGuiWindowFlags_HorizontalScrollbar);
-            if (copy) ImGui::LogToClipboard();
-
-            ImGui::TextUnformatted(Buf.begin());
-
-            if (ScrollToBottom) {
-                ImGui::SetScrollHere(1.0f);
-            }
-            ScrollToBottom = false;
-            ImGui::EndChild();
+        if(!ImGui::Begin(title, &visible)) {
+            visible = false;
             ImGui::End();
+            return;
         }
+
+        bool copy = ImGui::Button("Copy");
+        ImGui::SameLine();
+        ImGui::Separator();
+        ImGui::BeginChild("scrolling", ImVec2(0,0), false, ImGuiWindowFlags_HorizontalScrollbar);
+        if (copy) ImGui::LogToClipboard();
+
+        ImGui::TextUnformatted(Buf.begin());
+
+        if (ScrollToBottom) {
+            ImGui::SetScrollHere(1.0f);
+        }
+        ScrollToBottom = false;
+        ImGui::EndChild();
+        ImGui::End();
+
     }
 }
